fd5f9f3548e9cedafa1f82e7976e3965efa653a1
Nana-Kwame-A <kwameandw@gmail.com>
2023-06-08 16:17:08 -0700

    First Commit

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(x, y):
+    """My addition function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x + y
+    """
+    return (x + y)
