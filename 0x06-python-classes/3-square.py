fcc469928f4472ef875d87c45e9b01edc4717551
Nana-Kwame-A <kwameandw@gmail.com>
2023-06-27 16:58:45 -0700

    Mandatory Tasks

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """Represent a square."""
-
     def __init__(self, size=0):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
