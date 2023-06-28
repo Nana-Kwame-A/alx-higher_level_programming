e1d04772437877714d58a078d58693bb8c629f2a
Nana-Kwame-A <kwameandw@gmail.com>
2023-06-27 17:15:14 -0700

    Mandatory Tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,8 +4,10 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
@@ -17,7 +19,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
